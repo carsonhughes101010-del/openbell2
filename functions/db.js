39c6036da7a60d222e9cc7d0e8cc83d29cd4f337
carsonhughes101010-del <carsonhughes101010@gmail.com>
2026-03-29 12:33:27 -0500

    Update db.js

--- functions/db.js
+++ functions/db.js
@@ -1,3 +1,12 @@
+if (context.request.method === 'OPTIONS') {
+  return new Response(null, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type'
+    }
+  });
+}
 const headers = {
   'Content-Type': 'application/json',
   'Access-Control-Allow-Origin': '*'
